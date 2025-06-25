d6f5827e79f1d24732a840e123c0bddf7cb6af84
Scott Carr <kscarr73@gmail.com>
2025-06-25 07:02:06 -0500

    Add new makeHttpCall to return full HttpClientResponse

--- ApiModelsHelidonCommon/src/main/java/com/progbits/helidon/webclient/WebClientUtil.java
+++ ApiModelsHelidonCommon/src/main/java/com/progbits/helidon/webclient/WebClientUtil.java
@@ -15,6 +15,7 @@ import io.helidon.http.encoding.deflate.DeflateEncoding;
 import io.helidon.http.encoding.gzip.GzipEncoding;
 import io.helidon.webclient.api.ClientResponseTyped;
 import io.helidon.webclient.api.HttpClientRequest;
+import io.helidon.webclient.api.HttpClientResponse;
 import io.helidon.webclient.api.WebClient;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
@@ -219,13 +220,103 @@ public class WebClientUtil {
                         }
                     }
                 }
-
+                
                 resp = req.submit(sbForm.toString(), ApiObject.class);
             } else {
                 resp = req.submit(payload, ApiObject.class);
             }
         } else {
             resp = req.request(ApiObject.class);
+        }
+
+        return resp;
+    }
+    
+    public static HttpClientResponse makeHttpCall(WebClient client, String url, String method, String authorization, ApiObject props, ApiObject payload, boolean forceHttp1) {
+        HttpClientRequest req = client.method(Method.create(method));
+
+        if (forceHttp1) {
+            req.protocolId("http/1.1");
+        }
+        
+        if (url.startsWith("http:")) {
+            req.uri(url);
+        } else {
+            req.path(url);
+        }
+
+        if (authorization != null) {
+            req.header(HeaderNames.AUTHORIZATION, authorization);
+        }
+
+        if (props != null) {
+            if (props.isSet("headers")) {
+                for (var hdr : props.getObject("headers").keySet()) {
+                    if ("Content-Type".equals(hdr)) {
+                        req.contentType(MediaTypes.create(props.getObject("headers").getString(hdr)));
+                    }
+                    if ("Accept".equals(hdr)) {
+                        req.accept(MediaTypes.create(props.getObject("headers").getString(hdr)));
+                    } else {
+                        req.header(HeaderNames.create(hdr), props.getObject("headers").getString(hdr));
+                    }
+                }
+            }
+
+            if (props.isSet("params")) {
+                for (var prop : props.getObject("params").keySet()) {
+                    if (props.getObject("params").getType(prop) == ApiObject.TYPE_STRINGARRAY) {
+                        req.queryParam(prop, (String[]) props.getObject("params").getStringArray(prop).toArray());
+                    } else {
+                        req.queryParam(prop, props.getObject("params").getString(prop));
+                    }
+                }
+            }
+
+            if (!props.isSet("headers.Content-Type")) {
+                req.contentType(MediaTypes.APPLICATION_JSON);
+            }
+        } else {
+            req.contentType(MediaTypes.APPLICATION_JSON);
+        }
+
+        String flowId = MDC.get("flowid");
+
+        if (flowId != null) {
+            req.header(XFlowIdFilter.XFLOWID, flowId);
+        }
+
+        HttpClientResponse resp;
+
+        if (payload != null) {
+            if (payload.containsKey("formdata")) {
+                req.contentType(MediaTypes.APPLICATION_FORM_URLENCODED);
+
+                StringBuilder sbForm = new StringBuilder();
+                boolean isFirst = true;
+
+                for (var entry : payload.keySet()) {
+                    if (!"formdata".equals(entry)) {
+                        if (!isFirst) {
+                            sbForm.append("&");
+                        } else {
+                            isFirst = false;
+                        }
+
+                        Object objValue = payload.get(entry);
+
+                        if (objValue != null) {
+                            sbForm.append(entry).append("=").append(URLEncoder.encode(Objects.toString(objValue), Charset.forName("UTF-8")));
+                        }
+                    }
+                }
+                
+                resp = req.submit(sbForm.toString());
+            } else {
+                resp = req.submit(payload);
+            }
+        } else {
+            resp = req.request();
         }
 
         return resp;
